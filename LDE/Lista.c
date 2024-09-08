f08e8e57c3fee859e76ff49c0c4fabf6576f2cfd
Jaza Soares <jazasoares03@gmail.com>
2024-09-08 15:46:24 -0300

    Finalizado e testado com sucesso!

--- LDE/Lista.c
+++ LDE/Lista.c
@@ -65,7 +65,10 @@ void inserirElemento(ListaDuplamenteEncadeada* lista, int valor) {
 }
 
 void removerElemento(ListaDuplamenteEncadeada* lista, int valor) {
-    if (lista->inicio == NULL) return;
+    if (lista->inicio == NULL) {
+        printf("Lista vazia!\n");
+        return;
+    }
 
     No* atual = lista->inicio;
 
@@ -73,8 +76,10 @@ void removerElemento(ListaDuplamenteEncadeada* lista, int valor) {
         atual = atual->prox;
     }
 
-    if (atual == NULL) return; // Elemento não encontrado
-
+    if (atual == NULL) {
+        printf("Elemento nao encontrado na lista!!\n");
+        return; // Elemento não encontrado
+    }
     if (atual->ant != NULL) {
         atual->ant->prox = atual->prox;
     } else {
@@ -86,11 +91,11 @@ void removerElemento(ListaDuplamenteEncadeada* lista, int valor) {
     } else {
         lista->fim = atual->ant;
     }
-
     free(atual);
     lista->tamanho--;
-}
 
+    printf("Elemento removido com sucesso!\n");
+}
 int obterElementoPorPosicao(ListaDuplamenteEncadeada* lista, int posicao) {
     if (posicao < 0 || posicao >= lista->tamanho) {
         printf("Erro: Posição inválida\n");
@@ -106,6 +111,7 @@ int obterElementoPorPosicao(ListaDuplamenteEncadeada* lista, int posicao) {
 }
 
 int procurarElemento(ListaDuplamenteEncadeada* lista, int valor) {
+<<<<<<< Updated upstream
    No* atual = lista->inicio;
    int posicao = 0;
 
@@ -133,10 +139,34 @@ void imprimirLista(ListaDuplamenteEncadeada* lista){
 
     No* atual = lista->inicio;
     while (atual != NULL){
+=======
+    No* atual = lista->inicio;
+    int posicao = 0;
+
+    while (atual != NULL) {
+        if (atual->valor == valor) {
+            return posicao;
+        }
+        atual = atual->prox;
+        posicao++;
+    }
+
+    return -1;
+}
+
+int tamanhoLista(ListaDuplamenteEncadeada* lista) {
+    return lista->tamanho;
+}
+
+void imprimirLista(ListaDuplamenteEncadeada* lista) {
+    No* atual = lista->inicio;
+    while (atual != NULL) {
+>>>>>>> Stashed changes
         printf("%d ", atual->valor);
         atual = atual->prox;
     }
     printf("\n");
+<<<<<<< Updated upstream
 
 }
 
@@ -155,6 +185,19 @@ void imprimirListaReversa(ListaDuplamenteEncadeada* lista) {
 
 
 
+=======
+}
+
+void imprimirListaReversa(ListaDuplamenteEncadeada* lista) {
+    No* atual = lista->fim;
+    while (atual != NULL) {
+        printf("%d ", atual->valor);
+        atual = atual->ant;
+    }
+    printf("\n");
+}
+
+>>>>>>> Stashed changes
 void liberarLista(ListaDuplamenteEncadeada* lista) {
     No* atual = lista->inicio;
     while (atual != NULL) {
@@ -174,13 +217,13 @@ void menu() {
         printf("Menu:\n");
         printf("1. Inserir Elemento\n");
         printf("2. Remover Elemento por Valor\n");
-        printf("3. Obter Elemento por Posição\n");
+        printf("3. Obter Elemento por Posicao\n");
         printf("4. Procurar Elemento\n");
         printf("5. Tamanho da Lista\n");
         printf("6. Imprimir Lista\n");
         printf("7. Imprimir Lista Reversa\n");
         printf("0. Sair\n");
-        printf("Escolha uma opção: ");
+        printf("Escolha uma opcao: ");
         scanf("%d", &opc);
 
         system("cls || clear");
@@ -190,26 +233,25 @@ void menu() {
                 printf("Digite o valor a ser inserido: ");
                 scanf("%d", &valor);
                 inserirElemento(lista, valor);
-                printf("Elemento inserido com sucesso!!");
-                sleep(2);
+                printf("Elemento inserido com sucesso!!\n");
+                system("pause");
                 system("cls || clear");
                 break;
             case 2:
                 printf("Digite o valor a ser removido: ");
                 scanf("%d", &valor);
                 removerElemento(lista, valor);
-                printf("Elemento removido com sucesso!!");
-                sleep(2);
+                system("pause");
                 system("cls || clear");
                 break;
             case 3:
-                printf("Digite a posição: ");
+                printf("Digite a posicao: ");
                 scanf("%d", &posicao);
                 valor = obterElementoPorPosicao(lista, posicao);
                 if (valor != -1) {
-                    printf("Valor na posição %d: %d\n", posicao, valor);
+                    printf("Valor na posicao %d: %d\n", posicao, valor);
                 }
-                sleep(2);
+                system("pause");
                 system("cls || clear");
                 break;
             case 4:
@@ -217,28 +259,47 @@ void menu() {
                 scanf("%d", &valor);
                 posicao = procurarElemento(lista, valor);
                 if (posicao != -1) {
-                    printf("Valor encontrado na posição: %d\n", posicao);
+                    printf("Valor encontrado na posicao: %d\n", posicao);
                 } else {
+<<<<<<< Updated upstream
                     printf("Valor não encontrado\n");
                 }
                 sleep(2);
+=======
+                    printf("Valor nao encontrado\n");
+                }
+                system("pause");
+>>>>>>> Stashed changes
                 system("cls || clear");
                 break;
             case 5:
                 printf("Tamanho da lista: %d\n", tamanhoLista(lista));
+<<<<<<< Updated upstream
                 sleep(2);
+=======
+                system("pause");
+>>>>>>> Stashed changes
                 system("cls || clear");
                 break;
             case 6:
                 printf("Lista: ");
+<<<<<<< Updated upstream
                 imprimirLista(lista);
                 sleep(2);
+=======
+               imprimirLista(lista);
+                system("pause");
+>>>>>>> Stashed changes
                 system("cls || clear");
                 break;
             case 7:
                 printf("Lista (reversa): ");
                 imprimirListaReversa(lista);
+<<<<<<< Updated upstream
                 sleep(2);
+=======
+                system("pause");
+>>>>>>> Stashed changes
                 system("cls || clear");
                 break;
             case 0:
@@ -247,7 +308,7 @@ void menu() {
                 break;
             default:
                 printf("Opção inválida\n");
-                sleep(2);
+                system("pause");
                 system("cls || clear");
                 break;
         }
